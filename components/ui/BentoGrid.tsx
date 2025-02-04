acd733866add67a938110b1051325676dcb6077a
Yazeed Qasas <yazeedqasas12@gmail.com>
2025-02-04 23:59:55 +0200

    Fixed Tech Stack blocks

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import Lottie from 'react-lottie';
+
 
 export const BentoGrid = ({
   className,
@@ -42,10 +44,11 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
       style={{
@@ -86,11 +89,45 @@ export const BentoGridItem = ({
             <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
             {description}
             </div>
-            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 lg:text-2xl max-w-96 z-10">
           {title}
         </div>
+      
+
+      {id === 3 && (
+        <div className="flex gap-1 lg:gap-4 w-fit absolute right-1 pb-8">
+            <div className ="flex flex-col gap-3 pb-24 lg:gap-4 ">
+                  {['Java', 'NumPy','Pandas'].map(
+                    (item) => (
+                      <span key={item} className="py-4 lg:py4 lg:px-3 px-4 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-neutral-600 dark:text-neutral-200">
+                        {item}
+                      </span>
+                    ))}
+                    <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+              </div>
+
+              <div className ="flex flex-col gap-3 lg:gap-3">
+              <span className="py-6 px-4 rounded-lg text-center bg-[#10132E]" />
+
+                  {['Python', 'React.js','Javascript'].map(
+                    (item) => (
+                      <span key={item} className="py-4 lg:py4 lg:px-3 px-4 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E] text-neutral-600 dark:text-neutral-200">
+                        {item}
+                      </span>
+                    ))}
+              </div>
           </div>
+      ) }
+      {id === 6 && (
+        <div className="mt-5 relative">
+          <div className={`absolute' bottom-5 right-0`}>
+              
+          </div>
+          
         </div>
+      )}
     </div>
+  </div>
+  </div>
   );
 };
