e28a12a9bd1b48fe8fb10f29b2c255048928af08
Yazeed Qasas <yazeedqasas12@gmail.com>
2025-02-06 17:40:13 +0200

    Updated Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ import { FaGithub } from "react-icons/fa6";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36 -mb-60">
+    <div className="pb-20 pt-36 -mb-60 ">
         <div>
             <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill ="white"/>
             <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill ="purple"/>
