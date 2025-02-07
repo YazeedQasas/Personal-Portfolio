6efa5f57ce06005d868bc8c047d51d76ddbc9fcc
Yazeed Qasas <yazeedqasas12@gmail.com>
2025-02-07 13:30:04 +0200

    Deployment Measure

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['assets.aceternity.com', 'images.unsplash.com', 'i.imgur.com'],
+    unoptimized: true,
   },
+  assetPrefix: isProd ? '/portfolio-project/' : '',
+  basePath: isProd ? '/portfolio-project' : '',
+  output: 'export'
 };
+
 
 
 
